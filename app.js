5883a9be04017f2d10bcf36bceff55e5dbbfd5bd
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2022-08-04 22:43:32 +0800

    Update

---
+++ app.js
@@ -0,0 +1,7 @@
+// const navLink = document.querySelector('.nav-link');
+// const bar = document.querySelector('.navbar-nav');
+
+// $( '.navbar-nav a' ).on( 'click', function () {
+// 	$( '.navbar-nav' ).find( 'a.active' ).removeClass( 'active' );
+// 	$( this ).addClass( 'active' );
+// });
