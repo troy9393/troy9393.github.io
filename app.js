3f8e1fe13c2a260655bdf241d7182faf58d299ef
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2022-08-05 20:36:36 +0800

    Add animations

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-// const navLink = document.querySelector('.nav-link');
-// const bar = document.querySelector('.navbar-nav');
-
-// $( '.navbar-nav a' ).on( 'click', function () {
-// 	$( '.navbar-nav' ).find( 'a.active' ).removeClass( 'active' );
-// 	$( this ).addClass( 'active' );
-// });
+AOS.init();
